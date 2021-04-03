19140ace9f9b602d6834c7082e871e0f0693345f
Swistusmen <michal76aw@gmail.com>
2021-04-03 10:36:18 +0200

    Improving react

--- ferryman_ui/src/App.js
+++ ferryman_ui/src/App.js
@@ -4,28 +4,44 @@ import ResultTable from "./Components/ResultTable"
 import InputTable from "./Components/InputTable"
 import Output from "./Components/Output"
 import Submit from "./Components/Submit"
-import {useState } from 'react'
+import React, {useState, Component } from 'react'
 
 
-function App() {
-  const [showOutput,setShowOutput]=useState(false)
-  
 
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      myVar: 0,
+      showOutput: false
+    }
+    
+  };
+  setShowOutput=()=>{this.state.showOutput= !this.state.showOutput}
 
+  parentCallback=(v11)=>{
+      this.state.myVar= v11;
+      console.log(this.state.myVar);
+    }
+
+render(){
     return ( 
       <div className = "App" >
         <Header title={"Zadanie przewoznika"}/>
-        {!showOutput&&<Variable/>}
-        {!showOutput&&<InputTable/>}
-        {!showOutput&&<Submit onSubmit={()=>setShowOutput(!showOutput)} text={showOutput?"Reset":"Calculate"}
-        style={showOutput?"grey":"green"}/>}
-        {showOutput &&<ResultTable title="Tablica kosztow jednostkowych" o1="odbiorca" o2="odbiorca" o3="odbiorca" o4="odbiorca" d1="dostawca" d1="dostawca"
+        {!this.state.showOutput&&<Variable toFill={this.state.myVar} onCallback={this.parentCallback}/>}
+        {!this.state.showOutput&&<InputTable/>}
+        {!this.state.showOutput&&<Submit onSubmit={()=>this.setShowOutput(!this.state.showOutput)} text={this.state.showOutput?"Reset":"Calculate"}
+        style={this.state.showOutput?"grey":"green"}/>}
+        {this.state.showOutput &&<ResultTable title="Tablica kosztow jednostkowych" o1="odbiorca" o2="odbiorca" o3="odbiorca" o4="odbiorca" d1="dostawca" d1="dostawca"
         a1="0" a2="0" a3="0" a4="0" b1="0" b2="0" b3="0" b4="0"/>}
-        {showOutput &&<Output expense="0" income="0" profit="0"/>}
-        {showOutput&&<Submit onSubmit={()=>setShowOutput(!showOutput)} text={showOutput?"Reset":"Calculate"}
-        style={showOutput?"grey":"green"}/>}
+        {this.showOutput &&<Output expense="0" income="0" profit="0"/>}
+        {this.showOutput&&<Submit onSubmit={()=>this.setShowOutput(!this.state.showOutput)} text={this.state.showOutput?"Reset":"Calculate"}
+        style={this.state.showOutput?"grey":"green"}/>}
+        <button onClick={()=>{alert(this.state.myVar)}}/>
+          <h1>{this.state.myVar} </h1>
       </div>
     );
+}
 }
 
 export default App;
--- ferryman_ui/src/Components/Variable.js
+++ ferryman_ui/src/Components/Variable.js
@@ -1,6 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const Variable=(props)=>{
-    return(
+
+class Variable extends React.Component{
+    constructor(props){
+        super();
+        this.state={
+            v11: props.toFill,
+        };
+    }
+    myCallback=()=>{
+        this.setState({v11: this.state.v11});
+    }
+
+    updateValue(event){
+        this.setState({v11: event.target.value});
+        this.props.onCallback(event.target.value);
+    }
+
+    
+
+    render(){return(
         <div>
             <table className="table-component">
                 <tr>
@@ -14,25 +34,28 @@ const Variable=(props)=>{
                 </tr>
                 <tr>
                     <th>{"Popyt/Podaz"}</th>
-                    <th><input  type={Number} required="required"/></th>
-                    <th><input  type={Number} required="required"/></th>
-                    <th><input  type={Number} required="required"/></th>
-                    <th><input  type={Number} required="required"/></th>
-                    <th><input  type={Number} required="required"/></th>
-                    <th><input  type={Number} required="required"/></th>
+                    <th><input  id="p1" type={Number} required="required" 
+                    value={this.state.v11} onChange={this.updateValue.bind(this)}
+                    /></th>
+                    <th><input  id="p2" type={Number} required="required"/></th>
+                    <th><input  id="p3" type={Number} required="required"/></th>
+                    <th><input  id="p4" type={Number} required="required"/></th>
+                    <th><input  id="p5" type={Number} required="required"/></th>
+                    <th><input  id="p6" type={Number} required="required"/></th>
                 </tr>
                 <tr>
                     <th>{"Koszt/ Cena"}</th>
-                    <th><input  type={Number} required="required"/></th>
-                    <th><input  type={Number} required="required"/></th>
-                    <th><input  type={Number} required="required"/></th>
-                    <th><input  type={Number} required="required"/></th>
-                    <th><input  type={Number} required="required"/></th>
-                    <th><input  type={Number} required="required"/></th>
+                    <th><input  id="p7" type={Number} required="required"/></th>
+                    <th><input  id="p8" type={Number} required="required"/></th>
+                    <th><input  id="p9" type={Number} required="required"/></th>
+                    <th><input  id="p10" type={Number} required="required"/></th>
+                    <th><input  id="p11" type={Number} required="required"/></th>
+                    <th><input  id="p12" type={Number} required="required"/></th>
                 </tr>
             </table>
         </div>
     )
+}
 }
 
 export default Variable;
